4b074d48dd678a835acdf05036929ebddf26733f
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-08-10 19:56:25 +0300

    added checkout observer

--- HandleEvent/Model/Observer.php
+++ HandleEvent/Model/Observer.php
@@ -5,8 +5,11 @@ class Reachly_HandleEvent_Model_Observer
     {
         $cookie = Mage::getSingleton('core/cookie');
         if (!isset($_COOKIE['cart'])) {
-            $token = substr(md5(rand()), 0, 32);
-            $cookie->set('cart', $token, time() + 60 * 60 * 24 * 365 * 2, '/');
+            $cartToken = substr(md5(rand()), 0, 32);
+            $checkoutToken = substr(md5(rand()), 0, 32);
+
+            $cookie->set('cart', $cartToken, time() + 60 * 60 * 24 * 365 * 2, '/');
+            $cookie->set('checkout', $checkoutToken, time() + 60 * 60 * 24 * 365 * 2, '/');
         }
     }
 
@@ -16,9 +19,23 @@ class Reachly_HandleEvent_Model_Observer
         return $cookie->get('cart');
     }
 
-    public function processCartEvent($observer)
+    protected function getCheckoutToken()
     {
-        $cartArr = array();
+        $cookie = Mage::getSingleton('core/cookie');
+        return $cookie->get('checkout');
+    }
+
+    protected function timezone_offset_string($offset)
+    {
+            return sprintf( "%s%02d:%02d", ( $offset >= 0 ) ? '+' : '-', abs( $offset / 3600 ), abs( $offset % 3600 ) / 60 );
+    }
+
+    public function processCheckoutEvent($observer)
+    {
+        //Mage::log($this->getCartToken()."/".$this->getCheckoutToken());
+
+        $whArr = array();
+        $dataArr = array();
         $items   = array();
 
         $cart     = Mage::getModel('checkout/cart')->getQuote();
@@ -46,16 +63,52 @@ class Reachly_HandleEvent_Model_Observer
             }
         }
 
-        $cartArr["items"]             = $items;
-        $cartArr["token"]             = $this->getCartToken();
-        $cartArr["total_price"]       = $totaPrice;
-        $cartArr["total_weight"]      = $totaWeight;
-        $cartArr["item_count"]        = sizeof($allItems);
-        $cartArr["requires_shipping"] = false;
+        $dataArr["line_items"]             = $items;
+        $dataArr["cart_token"]             = $this->getCartToken();
+        $dataArr["token"]             = $this->getCheckoutToken();
+        $dataArr["total_price"]       = $totaPrice;
+        $dataArr["total_weight"]      = $totaWeight;
+        $dataArr["item_count"]        = sizeof($allItems);
+        $dataArr["currency"] = Mage::app()->getStore()->getCurrentCurrencyCode();
 
-        $json = json_encode($cartArr);
+        $whArr["data"] = $dataArr;
+        $whArr["topic"] = "checkouts/create";
 
-        $cartFile = fopen(Mage::getBaseDir()."/cart.json", "w");
-        fwrite($cartFile, $json);
+        $dt = new DateTime();
+        $formattedTime = $dt->format('Y-m-d').'T'.$dt->format('H:i:s').$this->timezone_offset_string(date_default_timezone_get());
+        $whArr["created_at"] = $formattedTime;
+        $whArr["updated_at"] = $formattedTime;
+
+        $json = json_encode($whArr);
+
+        $appID = "7a47d23ed6ae5fa5bd8697678d3f8b32632f8916";
+        $secretKey = "8e80f1a5b494b5150cb513fd332ba752cc6c481ae34ddcb3cf08e0b2dea256ba";
+
+        $auth = $appID.":".base64_encode(hash_hmac('sha256', $json, $secretKey));
+
+
+        $url = 'http://127.0.0.1:8042/checkout/';
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: application/json', 'Content-Length: '.strlen($json), 'Authorization: '.$auth));
+
+        $json_response = curl_exec($ch);
+        //$status = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        /*if ( $status != 201 ) {
+          Mage::log("Error: call to URL failed with status $status, response $json_response, curl_error " . curl_error($ch) . ", curl_errno " . curl_errno($ch));
+        }*/
+
+        curl_close($ch);
     }
 }
