ee39675aa708bdb6898921d75dce1b5bfb230fd3
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-08-12 16:54:01 +0300

    added script for zip extension archive generation

---
+++ zipExtension.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+mkdir -p tmp-install/app/etc/modules
+mkdir -p tmp-install/app/code/community/Reachly
+
+cp Reachly_All.xml tmp-install/app/etc/modules
+cp -r HandleEvent tmp-install/app/code/community/Reachly
+
+cd tmp-install
+zip -r ../Reachly.zip *
+cd ..
+rm -rf tmp-install
