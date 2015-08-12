43b41a972d9d2aa2f82c444e175dd24284b52b97
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-08-12 17:13:27 +0300

    added package information file

--- zipExtension.sh
+++ zipExtension.sh
@@ -3,6 +3,7 @@
 mkdir -p tmp-install/app/etc/modules
 mkdir -p tmp-install/app/code/community/Reachly
 
+cp package.xml tmp-install
 cp Reachly_All.xml tmp-install/app/etc/modules
 cp -r HandleEvent tmp-install/app/code/community/Reachly
 
