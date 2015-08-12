2c20e5113ddf3af34e8e8faeefefaa3d2b33aecc
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-08-12 16:02:20 +0300

    moved posting method into module helper

--- HandleEvent/Helper/Data.php
+++ HandleEvent/Helper/Data.php
@@ -43,4 +43,36 @@ class Reachly_HandleEvent_Helper_Data extends Mage_Core_Helper_Abstract
     {
         return "magento." . parse_url(Mage::getBaseUrl(), PHP_URL_HOST);
     }
+
+    public function postData($json, $endpoint)
+    {
+        $apiURL = 'http://127.0.0.1:8042';
+
+        $appID     = Mage::getStoreConfig('reachly_handleevent_options/section_one/field_app_id');
+        $secretKey = Mage::getStoreConfig('reachly_handleevent_options/section_one/field_secret_key');
+
+        $auth = $appID . ":" . base64_encode(hash_hmac('sha256', $json, $secretKey));
+
+        $url = $apiURL . '/' . $endpoint;
+        $ch  = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-type: application/json',
+            'Content-Length: ' . strlen($json),
+            'Authorization: ' . $auth
+        ));
+
+        curl_exec($ch);
+        curl_close($ch);
+    }
 }
