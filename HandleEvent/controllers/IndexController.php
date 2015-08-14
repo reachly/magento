a5f913914044a08b05e518b2a89b4aa62ff53fd0
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-08-14 15:32:43 +0300

    added reachly router for products and cart

---
+++ HandleEvent/controllers/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+class Reachly_HandleEvent_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function productsAction()
+    {
+
+    }
+
+    public function cartAction()
+    {
+
+    }
+}
