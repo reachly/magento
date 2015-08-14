e7766c1fb9faa7d66a9e28fbd21322cf5c024c1f
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-08-14 16:17:43 +0300

    added products' data into index controller

--- HandleEvent/controllers/IndexController.php
+++ HandleEvent/controllers/IndexController.php
@@ -3,7 +3,32 @@ class Reachly_HandleEvent_IndexController extends Mage_Core_Controller_Front_Act
 {
     public function productsAction()
     {
+        $helper = Mage::helper('reachly_handleevent');
 
+        $resultArr = array();
+        $prodArr   = array();
+
+        $collection = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('*');
+
+        foreach ($collection as $product) {
+            $dataArr = array();
+
+            $dataArr["id"]         = $product->getId();
+            $productName           = $product->getName();
+            $dataArr["title"]      = $productName;
+            $dataArr["handle"]     = $helper->getHandle($productName);
+            $timeArr               = $helper->getProductTimestamps($product);
+            $dataArr["created_at"] = $timeArr[0];
+            $dataArr["updated_at"] = $timeArr[1];
+
+            array_push($prodArr, $dataArr);
+        }
+
+        $resultArr["products"] = $prodArr;
+
+        $json = json_encode($resultArr);
+
+        echo ($json);
     }
 
     public function cartAction()
