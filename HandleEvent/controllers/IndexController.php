dd3ec48c65c5d76c26ec75e81915ec02f809b794
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-08-14 17:12:54 +0300

    added product tags to endpoint

--- HandleEvent/controllers/IndexController.php
+++ HandleEvent/controllers/IndexController.php
@@ -13,14 +13,24 @@ class Reachly_HandleEvent_IndexController extends Mage_Core_Controller_Front_Act
 
         foreach ($collection as $product) {
             $dataArr = array();
+            $tagsArr = array();
 
-            $dataArr["id"]         = $product->getId();
-            $productName           = $product->getName();
-            $dataArr["title"]      = $productName;
-            $dataArr["handle"]     = $helper->getHandle($productName);
-            $timeArr               = $helper->getProductTimestamps($product);
-            $dataArr["created_at"] = $timeArr[0];
-            $dataArr["updated_at"] = $timeArr[1];
+            $dataArr["id"]           = $product->getId();
+            $productName             = $product->getName();
+            $dataArr["title"]        = $productName;
+            $dataArr["handle"]       = $helper->getHandle($productName);
+            $timeArr                 = $helper->getProductTimestamps($product);
+            $dataArr["created_at"]   = $timeArr[0];
+            $dataArr["updated_at"]   = $timeArr[1];
+            $dataArr["product_type"] = $product->getTypeId();
+
+            $tagsModel = Mage::getModel('tag/tag');
+            $tags      = $tagsModel->getResourceCollection()->addPopularity()->addStatusFilter($tagsModel->getApprovedStatus())->addProductFilter($product->getId())->setFlag('relation', true)->addStoreFilter(Mage::app()->getStore()->getId())->setActiveFilter()->load()->getItems();
+
+            foreach ($tags as $tag) {
+                array_push($tagsArr, $tag->getName());
+            }
+            $dataArr["tags"] = $tagsArr;
 
             array_push($prodArr, $dataArr);
         }
