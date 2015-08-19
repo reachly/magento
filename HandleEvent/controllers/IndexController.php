842a3548eefddf25ad12e835eb4adb930a9edbaa
Aleksandrs Antonovs <alex.ant0303@gmail.com>
2015-08-19 15:49:39 +0300

    defining helper in constructor of index controller

--- HandleEvent/controllers/IndexController.php
+++ HandleEvent/controllers/IndexController.php
@@ -1,11 +1,14 @@
 <?php
 class Reachly_HandleEvent_IndexController extends Mage_Core_Controller_Front_Action
 {
+    protected function _construct()
+    {
+        $this->helper = Mage::helper('reachly_handleevent');
+    }
+
     //<store URL>/index.php/reachly/index/products
     public function productsAction()
     {
-        $helper = Mage::helper('reachly_handleevent');
-
         $resultArr = array();
         $prodArr   = array();
 
@@ -17,15 +20,15 @@ class Reachly_HandleEvent_IndexController extends Mage_Core_Controller_Front_Act
             $dataArr["id"]           = $product->getId();
             $productName             = $product->getName();
             $dataArr["title"]        = $productName;
-            $dataArr["handle"]       = $helper->getHandle($productName);
-            $timeArr                 = $helper->getProductTimestamps($product);
+            $dataArr["handle"]       = $this->helper->getHandle($productName);
+            $timeArr                 = $this->helper->getProductTimestamps($product);
             $dataArr["created_at"]   = $timeArr[0];
             $dataArr["updated_at"]   = $timeArr[1];
             $dataArr["product_type"] = $product->getTypeId();
-            $dataArr["tags"]         = $helper->getProductTags($product);
+            $dataArr["tags"]         = $this->helper->getProductTags($product);
             $dataArr["vendor"]       = $product->getAttributeText('manufacturer');
-            $dataArr["options"]      = $helper->getProductCustomOptions($product);
-            $dataArr["images"]       = $helper->getProductImages($product);
+            $dataArr["options"]      = $this->helper->getProductCustomOptions($product);
+            $dataArr["images"]       = $this->helper->getProductImages($product);
             //TODO: add variants
 
             array_push($prodArr, $dataArr);
@@ -41,12 +44,10 @@ class Reachly_HandleEvent_IndexController extends Mage_Core_Controller_Front_Act
     //<store URL>/index.php/reachly/index/cart
     public function cartAction()
     {
-        $helper = Mage::helper('reachly_handleevent');
-
         $resultArr = array();
 
-        $resultArr["cart_token"] = $helper->getCartToken();
-        $resultArr               = array_merge($resultArr, $helper->getCartData());
+        $resultArr["cart_token"] = $this->helper->getCartToken();
+        $resultArr               = array_merge($resultArr, $this->helper->getCartData());
 
         $json = json_encode($resultArr);
 
